b7f3f5af927c213d2fc4cc9c763121280a998cd0
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2024-02-08 12:49:25 -0500

    feat: :sparkles: added back to top button

--- script.js
+++ script.js
@@ -6,32 +6,35 @@ const navbarLinks = document.querySelectorAll("[data-nav-link]");
 const navbarToggler = document.querySelector("[data-nav-toggler]");
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
-const addEventOnElement = function(elem, type, callback){
-    if(elem.length > 1){
-        for(let i = 0; i < elem.length; i++){
+const addEventOnElement = function (elem, type, callback) {
+    if (elem.length > 1) {
+        for (let i = 0; i < elem.length; i++) {
             elem[i].addEventListener(type, callback);
         }
-    } else{
+    } else {
         elem.addEventListener(type, callback);
     }
 }
 
-const toggleNav = function(){
+const toggleNav = function () {
     navbar.classList.toggle("active");
     navbarToggler.classList.toggle("active");
 }
 
-const closeNav = function(){
+const closeNav = function () {
     navbar.classList.remove("active");
     navbarToggler.classList.remove("active");
 }
 
-window.addEventListener("scroll", function(){
-    if(this.window.scrollY >= 10){
+window.addEventListener("scroll", function () {
+    if (window.scrollY >= 100) {
         header.classList.add("active");
-    } else{
+        backTopBtn.classList.add("active");
+    } else {
         header.classList.remove("active");
+        backTopBtn.classList.remove("active");
     }
 })
 
